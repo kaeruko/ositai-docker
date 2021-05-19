c63a230ebc982fcf8c37545d4883c18151dca043
Your Name <mail@cloxs.jp>
2021-05-20 01:12:16 +0900

    a

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any','.*');
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
