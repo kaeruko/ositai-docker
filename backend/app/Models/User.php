6c261f00731b6ce8d9bd135dd1f8140114b71424
Your Name <mail@cloxs.jp>
2021-05-19 15:53:41 +0900

    a

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -2,15 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
-use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmailContract
+class User extends Authenticatable
 {
-    use HasFactory, Notifiable, MustVerifyEmail;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
